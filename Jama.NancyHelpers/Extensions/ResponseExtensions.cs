2d7a7e607a5520c2365c6942a0eab2d1590643e7
sltkr <random-spam@manghera.com>
2014-09-09 08:37:32 +0100

    Added cors extension, upped version to 1.0.4

---
+++ Jama.NancyHelpers/Extensions/ResponseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Jama.NancyHelpers.Extensions
+{
+    public static class ResponseExtensions
+    {
+        public static Response AddGlobalCors(this Response response)
+        {
+            return response
+                .WithHeader("Access-Control-Allow-Origin", "*")
+                .WithHeader("Access-Control-Allow-Methods", "POST,GET")
+                .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+        }
+    }
+}
