68673b39b8b787a92d6931a689c55e47b112b7a2
sltkr <random-spam@manghera.com>
2015-05-19 09:03:16 +0100

    Added custom databag, upped version to 1.0.6.0

--- Jama.NancyHelpers/Models/DomainResponse.cs
+++ Jama.NancyHelpers/Models/DomainResponse.cs
@@ -6,12 +6,17 @@ namespace Jama.NancyHelpers.Models
     public class DomainResponse
     {
         private List<Error> _errors = new List<Error>();
+        private Dictionary<string, string> _dataBag = new Dictionary<string, string>(); 
 
         public bool HasErrors { get { return Errors.Any() || !string.IsNullOrEmpty(ErrorType); } }
 
         public void AddError(string errorMessage)
         {
             _errors.Add(new Error("Custom Error", errorMessage));
+        }
+        public void AddDataBagEntry(string key, string value)
+        {
+            _dataBag.Add(key, value);
         }
 
         public List<Error> Errors
@@ -21,6 +26,12 @@ namespace Jama.NancyHelpers.Models
         }
 
         public string ErrorType { get; set; }
+
+        public Dictionary<string, string> DataBag
+        {
+            get { return _dataBag; }
+            set { _dataBag = value; }
+        }
     }
 
     public class DomainResponse<T> : DomainResponse where T : class
